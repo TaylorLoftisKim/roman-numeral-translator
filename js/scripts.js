bdcec4639ee23ba9f714651ceb2ab81699281be4
Ryan Peterson and taylor loftis-kim <ryan@>
2016-10-27 10:43:13 -0700

    add function to evaluate ones place

--- js/scripts.js
+++ js/scripts.js
@@ -19,15 +19,32 @@ var valueCheck = function(number){
 }
 
 var translator = function(number){
+
+    var romanArray = ['I','X','C','D','M'];
     var tempString = number.toString();
     var tempArray = tempString.split("");
+    var outputArray = [];
+    var counter = 0;
 
     numberArray = tempArray.map(function(num){
       return parseInt(num);
     });
-    return numberArray;
 
+    var length = numberArray.length;
+    for(var numIndex = length; numIndex >= 0; numIndex--){
+      console.log(numIndex);
+      for(var romanIndex = 0; romanIndex < romanArray.length; romanIndex++){
+        while(counter < numberArray[numIndex]){
+          outputArray.push(romanArray[romanIndex]);
+          counter++;
+        }
+      }
+
+    }
+    return outputArray;
 }
+
+
 
 $(document).ready(function(){
   $("#roman-numeral").submit(function(){
